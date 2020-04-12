a1a336a88cf2a6fde0c3c765f33abf396b8456fe
Ignacio Arias <1992ignacio@gmail.com>
2020-04-12 13:35:55 -0700

    Localizable strings added, center text, text style, refactor, icon to system.

--- Meme1.0/ViewController.swift
+++ Meme1.0/ViewController.swift
@@ -8,77 +8,114 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+						UINavigationControllerDelegate, UITextFieldDelegate {
 
 	@IBOutlet weak var img: UIImageView!
 
-	@IBOutlet weak var tf1: UITextField!
-	@IBOutlet weak var tf2: UITextField!
+	@IBOutlet weak var topTexField: UITextField!
+	@IBOutlet weak var bottomTextField: UITextField!
 	@IBOutlet weak var cameraBtn: UIBarButtonItem!
 
+	@IBOutlet weak var shareBtn: UIBarButtonItem!
 
-	@objc func share() {
-		let image = generateMemedImage()
-		let activityView = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+	 private func share() {
+		let memedImage = generateMemedImage()
+		let activityView = UIActivityViewController(activityItems: [memedImage],
+													applicationActivities: nil)
+		activityView.completionWithItemsHandler = { [weak self] type, completed, items, error in
+			if completed {
+				self?.save(memedImage)
+			}
+			activityView.dismiss(animated: true, completion: nil)
+		}
 		present(activityView, animated: true, completion: nil)
 	}
 
-	private var shareBtn: UIBarButtonItem? // This will be initialized in configureNavBar
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-
-		shareBtn = UIBarButtonItem(image: UIImage(named: "share"), style: .plain, target: self, action: #selector(share))
-
-		navigationItem.setLeftBarButtonItems([shareBtn!], animated: true)
-
-		cameraBtn.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-
-		tf1.delegate = self
-		tf2.delegate = self
-
+	fileprivate func setUpTextFieldAttributedStrings() {
 		let memeTextAttributes: [NSAttributedString.Key: Any] = [
-			NSAttributedString.Key.strokeColor: UIColor.red,
-			NSAttributedString.Key.foregroundColor: UIColor.orange,
-			NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+			NSAttributedString.Key.strokeColor: UIColor.black,
+			NSAttributedString.Key.foregroundColor: UIColor.white,
+			NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack",
+												size: 40)!,
 			NSAttributedString.Key.strokeWidth:  3
 		]
 
-		tf1.defaultTextAttributes = memeTextAttributes
-		tf2.defaultTextAttributes = memeTextAttributes
+		topTexField.defaultTextAttributes = memeTextAttributes
+		bottomTextField.defaultTextAttributes = memeTextAttributes
+
+		let paragraphStyle = NSMutableParagraphStyle()
+		paragraphStyle.alignment = .center
+		topTexField.attributedText = NSAttributedString(string: topTexField.placeholder!, attributes: [.paragraphStyle: paragraphStyle])
+
+		bottomTextField.attributedText = NSAttributedString(string: bottomTextField.placeholder!, attributes: [.paragraphStyle: paragraphStyle])
+
+
+		topTexField.textAlignment = .center
+		bottomTextField.textAlignment = .center
+
+
+
 
 	}
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		topTexField.delegate = self
+		bottomTextField.delegate = self
+	}
+
+	@IBAction func handleShare(_ sender: Any) {
+		share()
+	}
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		cameraBtn.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+
+
+		subscribeToKeyboardNotifications()
+
+		topTexField.isHidden = true
+		bottomTextField.isHidden = true
+
+		cancelBtn.isEnabled = false
+		shareBtn?.isEnabled = false
+
+		activateShare()
+	}
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		setUpTextFieldAttributedStrings()
+	}
+
+
+
 	func activateShare() {
-		if tf1.text!.count > 0 || tf2.text!.count > 0 {
-		shareBtn?.isEnabled = true
+		if topTexField.text!.count > 0 || bottomTextField.text!.count > 0 {
+			shareBtn?.isEnabled = true
 			cancelBtn.isEnabled = true
 		}
 	}
 
-	override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(animated)
-		subscribeToKeyboardNotifications()
-		cancelBtn.isEnabled = false
-		tf1.isHidden = true
-		tf2.isHidden = true
-		shareBtn?.isEnabled = false
-		activateShare()
-	}
+
 
 	func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-		navigationItem.title = "Add text :)"
+		navigationItem.title = NSLocalizedString("navBarAddText", comment: "Adding text")
 		return true
 	}
 
 	func textFieldDidBeginEditing(_ textField: UITextField) {
-			textField.text = ""
+		textField.text = ""
 	}
 
 	func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
 		if textField.text!.count > 0 {
 			shareBtn?.isEnabled = true
-			navigationItem.title = "Share it :)"
+			textField.textAlignment = .center
+			navigationItem.title = NSLocalizedString("navBarShare", comment: "Sharing")
 		}
 		return true
 	}
@@ -96,12 +133,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 	}
 
 
-	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+	func imagePickerController(_ picker: UIImagePickerController,
+							   didFinishPickingMediaWithInfo info:
+								[UIImagePickerController.InfoKey : Any]) {
 
 		//add the image to the imageView
 		if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-				   img.image = image
-			   }
+			img.image = image
+		}
 		//Dismissing the selected image and closing the presenter
 		dismiss(animated: true, completion: nil)
 	}
@@ -113,29 +152,30 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
 	fileprivate func setUp() {
 		cancelBtn.isEnabled = true
-		tf1.isHidden = false
-		tf2.isHidden = false
+		topTexField.isHidden = false
+		bottomTextField.isHidden = false
 	}
 
-	@IBAction func cam(_ sender: Any) {
-		let cmpiker = UIImagePickerController()
-		cmpiker.delegate = self
-		cmpiker.sourceType = .camera
-		present(cmpiker, animated: true, completion: nil)
-
+	func pickFromSource(_ source: UIImagePickerController.SourceType) {
+		let imagePickerController = UIImagePickerController()
+		imagePickerController.delegate = self;
+		imagePickerController.sourceType = source
+		present(imagePickerController, animated: true, completion: nil)
 		setUp()
 	}
-	@IBAction func add(_ sender: Any) {
-		let imgPicker = UIImagePickerController()
-		imgPicker.delegate = self
-		imgPicker.sourceType = .photoLibrary
-		present(imgPicker, animated: true, completion: nil)
-		setUp()
+
+
+	@IBAction func addFromCamera(_ sender: Any) {
+		pickFromSource(.camera)
+	}
+
+	@IBAction func addFromGallery(_ sender: Any) {
+		pickFromSource(.photoLibrary)
 	}
 
 	@objc func keyboardWillShow(_ notification:Notification) {
-		if tf2.isFirstResponder {
-		   view.frame.origin.y = -getKeyboardHeight(notification)
+		if bottomTextField.isFirstResponder {
+			view.frame.origin.y = -getKeyboardHeight(notification)
 		}
 	}
 
@@ -146,42 +186,60 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 	func getKeyboardHeight(_ notification:Notification) -> CGFloat {
 
 		let userInfo = notification.userInfo
-		let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
-		print("KeyboardSize: \(keyboardSize.cgRectValue.height)")
+		let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
 		return keyboardSize.cgRectValue.height
 	}
+
 	@IBAction func cancelResent(_ sender: Any) {
 		img.image = nil
-		tf1.placeholder = "TOP"
-		tf2.placeholder = "BOTTOM"
-		tf1.text = ""
-		tf2.text = ""
-		navigationItem.title = "Please, add an image :)"
+		topTexField.placeholder = NSLocalizedString("placeholder_Text",
+													comment: "Text for placeholder")
+
+		bottomTextField.placeholder = NSLocalizedString("placeholder_Text",
+														comment: "Text for placeholder")
+		topTexField.text = ""
+		bottomTextField.text = ""
+		navigationItem.title = NSLocalizedString("navBarAddImage", comment: "Adding an image")
 		cancelBtn.isEnabled = false
 		shareBtn?.isEnabled = false
+
+		topTexField.isHidden = true
+		bottomTextField.isHidden = true
 	}
 
 
 
 	func subscribeToKeyboardNotifications() {
-		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
-		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+											   name: UIResponder.keyboardWillHideNotification,
+											   object: nil)
+
+		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)),
+											   name: UIResponder.keyboardWillShowNotification,
+											   object: nil)
 	}
 
 	func unsubscribeFromKeyboardNotifications() {
-		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
-		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+		NotificationCenter.default.removeObserver(self,
+												  name: UIResponder.keyboardWillHideNotification,
+												  object: nil)
+
+		NotificationCenter.default.removeObserver(self,
+												  name: UIResponder.keyboardWillShowNotification,
+												  object: nil)
 	}
 
-
-
-	func save() {
-		let meme = Meme(topText: tf1.text!, bottomText: tf2.text!, originalImage: img.image!, memeImage: generateMemedImage())
-	}
+	func save(_ memedImage: UIImage) {
+        _ = Meme(topText: topTexField.text!,
+				 bottomText: bottomTextField.text!,
+				 originalImage: img.image!,
+				 memeImage: memedImage)
+    }
 
 
 	@IBOutlet weak var cancelBtn: UIBarButtonItem!
 	@IBOutlet weak var toolbar: UIToolbar!
+
 	func generateMemedImage() -> UIImage {
 
 		// TODO: Hide toolbar and navbar
